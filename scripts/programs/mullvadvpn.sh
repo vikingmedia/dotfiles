f18bb03cc97bd30e1aedf37ea758b16a9dbbe7e5
vikingmedia <erik.seethaler@vikingmedia.at>
2024-10-18 17:09:29 +0200

    add mullvad vpn

---
+++ scripts/programs/mullvadvpn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "🕵️‍♀️ Installing Mullvad VPN"
+# Download the Mullvad signing key
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+
+# Add the Mullvad repository server to apt
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+
+# Install the package
+sudo apt update
+sudo apt install mullvad-vpn
