aa0989c835c8bf593a54816edebbf96ba050daa7
vikingmedia <erik.seethaler@vikingmedia.at>
2026-01-23 17:39:24 +0100

    update anydesk

--- scripts/programs/anydesk.sh
+++ scripts/programs/anydesk.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "👀 Installing AnyDesk"
-curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY|sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/anydesk.gpg
-echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk.list
+sudo curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
+sudo chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
+echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null
 sudo apt update
 sudo apt install anydesk -y
-
-
