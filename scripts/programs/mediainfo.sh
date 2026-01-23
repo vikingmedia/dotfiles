148e06e90ef49e2045a256ba97271f29e7d370a0
vikingmedia <erik.seethaler@vikingmedia.at>
2026-01-23 17:29:34 +0100

    update Mediainfo repo from 0.24 --> 0.26

--- scripts/programs/mediainfo.sh
+++ scripts/programs/mediainfo.sh
@@ -2,9 +2,7 @@
 
 echo "👁️ Installing Mediainfo"
 cd /tmp
-wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-24_all.deb
-sudo dpkg -i repo-mediaarea_1.0-24_all.deb
+wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-26_all.deb 
+dpkg -i repo-mediaarea_1.0-26_all.deb 
 apt update
 sudo apt install mediainfo-gui -y
-
-
